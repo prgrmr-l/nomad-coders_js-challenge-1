d34e440b1b8e8326316baac6bae9cbcd8f6a9b49
yougotmyphone <110523886+yougotmyphone@users.noreply.github.com>
2022-09-02 20:45:48 +0900

    challenge-2

--- index.js
+++ index.js
@@ -1,30 +1,13 @@
-const colors = ["#1abc9c", "#3498db", "#9b59b6", "#f39c12", "#e74c3c"];
-
-const superEventHandler = document.querySelector("h2");
-
-superEventHandler.style.color = colors[0];
-
-function mouseEnter() {
-  superEventHandler.innerText = "Hi";
-  superEventHandler.style.color = colors[1];
-}
-
-function mouseOut() {
-  superEventHandler.innerText = "Hello";
-  superEventHandler.style.color = colors[2];
-}
+const colors = ["#1abc9c", "#3498db", "#9b59b6"];
+document.body.style.backgroundColor = colors[0];
 
 function reSize() {
-  superEventHandler.innerText = "Hola";
-  superEventHandler.style.color = colors[3];
+  const width = window.innerWidth;
+  if (width > 1200) {
+    document.body.style.backgroundColor = colors[0];
+  } else if (width < 1200 && width > 600) {
+    document.body.style.backgroundColor = colors[1];
+  } else document.body.style.backgroundColor = colors[2];
 }
 
-function contextMenu() {
-  superEventHandler.innerText = "Hola!!!";
-  superEventHandler.style.color = colors[4];
-}
-
-superEventHandler.addEventListener("mouseenter", mouseEnter);
-superEventHandler.addEventListener("mouseout", mouseOut);
 window.addEventListener("resize", reSize);
-superEventHandler.addEventListener("contextmenu", contextMenu);
